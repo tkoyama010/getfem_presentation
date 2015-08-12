ecb710bdaaff305aad4bfd09837b6faa8ac223b2
Tetsuo Koyama <tkoyama010@gmail.com>
2015-08-13 00:47:11 +0900

    C++によるサンプルファイルを追加

---
+++ src/demo_thermo_elasticity_electrical_coupling.cc
@@ -0,0 +1,10 @@
+#include "getfem/getfem_assembling.h"
+#include "getfem/getfem_export.h"
+#include "getfem/getfem_regular_meshes.h"
+#include "getfem/getfem_model_solvers.h"
+#include "getfem/getfem_nonlinear_elasticity.h"
+#include "getfem/getfem_superlu.h"
+#include "gmm/gmm.h"
+
+int main(void) {
+}
