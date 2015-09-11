17c3a8dbc014101f0f9bb820a73f162db91e9092
Tetsuo Koyama <tkoyama010@gmail.com>
2015-09-11 22:27:27 +0900

    デモ用ファイル一式を追加

---
+++ demo/exec_install.bat
@@ -0,0 +1,5 @@
+.\installer\python-2.7.10.msi
+.\installer\numpy-1.9.2-win32-superpack-python2.7.exe
+.\installer\scipy-0.16.0-win32-superpack-python2.7.exe
+.\installer\getfem_python-4.1.win32-py2.7.exe
+pause
---
+++ demo/exec_ipython.bat
@@ -0,0 +1,4 @@
+PATH=%PATH%;C:\Python27;C:\Python27\Scripts
+pip install notebook
+ipython notebook
+pause
